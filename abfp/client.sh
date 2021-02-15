7be81ebe96b3765193fa975db2c250ac6983d168
Gerard Benito <gerard.benito@enti.cat>
2021-02-15 23:48:01 +0100

    MD5 bash

--- abfp/client.sh
+++ abfp/client.sh
@@ -5,31 +5,77 @@ PORT=2021
 IP_CLIENT="127.0.0.1"
 IP_SERVER="127.0.0.1"
 
+FILE_NAME="archivo_salida.vaca"
+
 echo "Cliente de ABFP"
 
-echo "(2) Sending Headers"
+echo "(2) SENDING HEADERS"
 
 echo "ABFP $IP_CLIENT" | nc -q 1 $IP_SERVER $PORT
 
-echo "(3) Listening $PORT"
+echo "(3) LISTEN CONNECTION RESPONSE"
 
 RESPONSE=`nc -l -p $PORT`
+
 if [ "$RESPONSE" != "OK_CONN" ]; then
 	echo "No se ha podido conectar con el servidor"
 	exit 1
 fi
 
-echo "(6) Handshake" 
+echo "(6) SENDING HANDSHAKE" 
 
 sleep 1
 echo "THIS_IS_MY_CLASSROOM" | nc -q 1 $IP_SERVER $PORT
 
-echo "(7) Listening $PORT"
 
-RESPONSE2=`nc -l -p $PORT`
-if [ "$RESPONSE2" != "YES_IT_IS" ]; then
-	echo "No se ha podidio conectar con el servidor"
-	exit 1
+echo "(7) LISTEN HANDSHAKE"
+
+RESPONSE=`nc -l -p $PORT`
+
+if [ "$RESPONSE" != "YES_IT_IS" ]; then
+	echo "No se ha recibido correctamente el Handshake"
+
+	exit 2
 fi
+
+
+echo "(10) SENDING FILE_NAME"
+
+
+sleep 1
+echo "FILE_NAME $FILE_NAME" | nc -q 1 $IP_SERVER $PORT
+
+sleep 1 
+
+FILE_MD5=`echo -n $FILE_NAME | md5sum`
+echo "FILE_MD5 $FILE_MD5" | nc -q 1 $IP_SERVER $PORT
+
+echo "(11) LISTEN FILE_NAME RESPONSE"
+
+RESPONSE=`nc -l -p $PORT`
+
+echo "TEST FILE_NAME RESPONSE"
+
+if [ "$RESPONSE" != "OK_FILE_NAME" ]; then
+# echo "Error al enviar el nombre del archivo"
+
+	exit 3
+fi
+
+echo "TEST FILE_MD5 RESPONSE"
+
+if [ "$RESPONSE" != "OK" ]; then
+	echo "Error al enviar el MD5 del archivo"
+
+	exit 4
+fi
+
+echo "(14) SENDING DATA"
+
+sleep 1
+cat $FILE_NAME | nc -q l $IP_SERVER $PORT
+
+sleep 1
+cat $FILE_MD5 | nc -q 1 $IP_SERVER $PORT 
 
 exit 0
--- abfp/server.sh
+++ abfp/server.sh
@@ -2,18 +2,17 @@
 
 PORT=2021
 
-echo "(0) Server ABFP"
+echo "(0) SERVER ABFP"
 
-echo "(1) Listen $PORT"
+echo "(1) LISTENING ($PORT) HEADERS"
 
 HEADER=`nc -l -p $PORT`
 
-echo "Test! $HEADER"
 
 PREFIX=`echo $HEADER | cut -d " " -f 1`
 IP_CLIENT=`echo $HEADER | cut -d " " -f 2`
 
-echo "(4) Response"
+echo "TEST HEADER"
 
 if [ "$PREFIX" != "ABFP" ]; then
 
@@ -25,28 +24,75 @@ if [ "$PREFIX" != "ABFP" ]; then
 	exit 1
 fi
 
+echo "(4) RESPONSE HEADER"
+
 sleep 1
 echo "OK_CONN" | nc -q 1 $IP_CLIENT $PORT
 
-echo "(5) Listening $PORT"
+echo "(5) LISTEN HANDSHAKE"
 
 HANDSHAKE=`nc -l -p $PORT`
 
-echo "(8) Response"
+echo "TEST HANDSHAKE"
 
-if [ "$PREFIX" != "ABFP" ]; then
+if [ "$HANDSHAKE" != "THIS_IS_MY_CLASSROOM" ]; then
 
 	echo "Error en la cabecera"
 	
 	sleep 1
 	echo "KO_HANDSHAKE" | nc -q 1 $IP_CLIENT $PORT
 
-	exit 1
+	exit 2
 fi
+
+echo "(8) RESPONSE HANDSHAKE"
 
 sleep 1
 echo "YES_IT_IS" | nc -q 1 $IP_CLIENT $PORT
 
-echo "(9) Listening $PORT"
+echo "(9) LISTEN FILE_NAME"
+
+FILE_NAME=`nc -l -p $PORT`
+
+PREFIX=`echo $FILE_NAME | cut -d " " -f 1`
+NAME=`echo $FILE_NAME | cut -d " " -f 2`
+
+echo "TEST FILE_NAME"
+
+if [ "$PREFIX" != "FILE_NAME" ]; then
+
+	echo "Error en el nombre de archivo"
+
+	sleep 1
+	echo "KO_FILE_NAME" | nc -q 1 $IP_CLIENT $PORT
+
+	exit 3
+fi
+
+MD5=`echo $FILE_NAME | md5sum`
+
+#if [ "$MD5" != "FILE_NAME" ]; then
+
+# echo "Error en el MD5 de archivo"
+
+#	sleep 1
+#	echo "KO" | nc -q 1 $IP_CLIENT $PORT
+
+#	exit 4
+#fi
+
+echo "(12) RESPONSE FILE_NAME ($NAME)"
+
+sleep 1
+#echo "OK_FILE_NAME" | nc -q 1 $IP_CLIENT $PORT
+
+echo "(12.5) RESPONSE FILE_MD5 ($MD5)"
+
+sleep 1
+echo "OK" | nc -q 1 $IP_CLIENT $PORT
+
+echo "(13) LISTEN DATA"
+
+nc -l -p $PORT > archivo_entrada.vaca
 
 exit 0
